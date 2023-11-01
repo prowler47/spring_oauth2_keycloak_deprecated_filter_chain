61be3b17db575448457c3330e4849e304078a133
prowler47 <sdragunovskiy@gmail.com>
2023-11-01 12:57:45 +0200

    added logback.xml file and logging property in properties file

--- src/main/java/ua/dragunovskiy/config/SecurityConfig.java
+++ src/main/java/ua/dragunovskiy/config/SecurityConfig.java
@@ -5,11 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2ClientConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
 import ua.dragunovskiy.filter.TestFilter;
+
 
 @Configuration
 @EnableWebSecurity
